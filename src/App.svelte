52e68b783aa6444c0efe4d793cfd7d7b9a8e2400
root <root@DESKTOP-5VPSV7M.localdomain>
2021-08-24 11:37:29 -0400

    fix reset button refreshing page

--- src/App.svelte
+++ src/App.svelte
@@ -36,40 +36,38 @@
 <main>
 	<div class="container">
 		<h1>Eating Out Bill Splitter</h1>
-		<form>
-			<p class="large"><Fa icon={faCreditCard} color="#2759DB" /> Total amount:</p>
-			<span class="dollar">
-				<input type="number" bind:value={cost} min="0" step="0.01" readonly={!calcNotFinished} style="width:300px;" />
+		<p class="large"><Fa icon={faCreditCard} color="#2759DB" /> Total amount:</p>
+		<span class="dollar">
+			<input type="number" bind:value={cost} min="0" step="0.01" readonly={!calcNotFinished} style="width:300px;" />
+		</span>
+		<p class="large"><Fa icon={faUsers} color="#2759DB" /> Number of people:</p>
+		<input type="number" bind:value={people} min="1" step="1" readonly={!calcNotFinished} style="width:300px;" />
+
+		{#if tips}
+			<p class="large tip-text"><Fa icon={faHeart} color="#2759DB" /> <span class="highlight pointer" on:click="{invertTip}">Close tip menu</span></p>
+			<br>
+			<span class="percent">
+				<input type="number" bind:value={tip} min="0" max="100" readonly={!calcNotFinished} style="width:300px;" />
 			</span>
-			<p class="large"><Fa icon={faUsers} color="#2759DB" /> Number of people:</p>
-			<input type="number" bind:value={people} min="1" step="1" readonly={!calcNotFinished} style="width:300px;" />
+			<br>
+			<br>
+		{:else}
+			<p class="large"><Fa icon={faHeart} color="#2759DB" /> Feeling generous? <span class="highlight pointer" on:click="{invertTip}" >Add a tip</span></p>
+		{/if}
 
-			{#if tips}
-				<p class="large tip-text"><Fa icon={faHeart} color="#2759DB" /> <span class="highlight pointer" on:click="{invertTip}">Close tip menu</span></p>
-				<br>
-				<span class="percent">
-					<input type="number" bind:value={tip} min="0" max="100" readonly={!calcNotFinished} style="width:300px;" />
-				</span>
-				<br>
-				<br>
-			{:else}
-				<p class="large"><Fa icon={faHeart} color="#2759DB" /> Feeling generous? <span class="highlight pointer" on:click="{invertTip}" >Add a tip</span></p>
-			{/if}
-
-			{#if calcNotFinished}
-				<button class="btn btn-primary" on:click={splitMoney}>Split!</button>
+		{#if calcNotFinished}
+			<button class="btn btn-primary" on:click={splitMoney}>Split!</button>
+			<button class="btn" on:click={resetValues}>Reset</button>
+		{:else}
+			{#if errored}
+				<p class="error">The amount, number of people, and tip must all be valid numbers!</p>
 				<button class="btn" on:click={resetValues}>Reset</button>
 			{:else}
-				{#if errored}
-					<p class="error">The amount, number of people, and tip must all be valid numbers!</p>
-					<button class="btn" on:click={resetValues}>Reset</button>
-				{:else}
-					<p class="x-large">Everyone's share is: <span class="highlight">${total}</span></p>
-					<button class="btn" on:click={resetValues}>Reset</button>
-					<button class="btn btn-primary" on:click={copyText}><Fa icon={faShareAlt} color="white" /></button>
-				{/if}
+				<p class="x-large">Everyone's share is: <span class="highlight">${total}</span></p>
+				<button class="btn" on:click={resetValues}>Reset</button>
+				<button class="btn btn-primary" on:click={copyText}><Fa icon={faShareAlt} color="white" /></button>
 			{/if}
-		</form>
+		{/if}
 	</div>
 </main>
 
